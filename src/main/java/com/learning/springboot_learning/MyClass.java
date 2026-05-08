f6a399c18fed37006450674bb86a13e8af3ef164
Saptarshi-26 <saptarshiok26@gmail.com>
2026-05-08 21:31:27 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/learning/springboot_learning/MyClass.java
@@ -0,0 +1,11 @@
+package com.learning.springboot_learning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class MyClass {
+    @GetMapping("/hello")
+    public String hello(){
+        return  "hello";
+    }
+}
